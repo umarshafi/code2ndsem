ad67ae688fc05cb8ca9cc6960f40f5a5d60312ec
umarshafi <148948917+umarshafi@users.noreply.github.com>
2025-04-09 18:46:55 +0530

    Create quicksort.c

---
+++ quicksort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int partition(int arr[],int low,int high){
+int pivot=arr[high];
+int i=low-1
+for(int j=low;j<high;j++){
+if(arr[j]<=pivot){
+int temp=arr[i];
+arr[j]=temp;
+swap arr[i+1]& arr[high]{
+
+int temp=arr[i+1];
+arr[i+1]=arr[high];
+arr[high]=temp;
+return i+1;
+int main(){ int arr[]={10,7,8,9,1,5}
+int n=sizeof(arr)/sizeofarr[0];
+printf("array");
+return 0;
